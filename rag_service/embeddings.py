59b0f36ecf99cc159fa16490298cdd9ae1eea952
NehaVishwakarma2607 <nehavishwakarma2607@gmail.com>
2026-02-20 12:26:53 +0530

    removed hugging face embedding replaced with openai embedding

--- rag_service/embeddings.py
+++ rag_service/embeddings.py
@@ -1,7 +1,3 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
-embedding_model=HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-
-##vector size =384
+from langchain_openai import OpenAIEmbeddings
+embedding_model=OpenAIEmbeddings()
