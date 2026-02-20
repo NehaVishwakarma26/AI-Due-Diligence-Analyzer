f690ac16ec10374c987190950471e4e523afaf45
NehaVishwakarma2607 <nehavishwakarma2607@gmail.com>
2026-02-20 12:41:36 +0530

    req.txt updated

--- rag_service/ingestion.py
+++ rag_service/ingestion.py
@@ -1,4 +1,4 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from urllib3.util.request import ChunksAndContentLength
 from embeddings import embedding_model
