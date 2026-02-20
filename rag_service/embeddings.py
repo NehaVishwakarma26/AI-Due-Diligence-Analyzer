d6a8487f8cede9143f7268b4fcdc99715ad00b6e
NehaVishwakarma2607 <nehavishwakarma2607@gmail.com>
2026-02-20 13:42:57 +0530

    fixed

--- rag_service/embeddings.py
+++ rag_service/embeddings.py
@@ -1,2 +1,6 @@
-from langchain_openai import OpenAIEmbeddings
-embedding_model=OpenAIEmbeddings()
+from langchain_community.embeddings import HuggingFaceEmbeddings
+embedding_model=HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+##vector size =384
